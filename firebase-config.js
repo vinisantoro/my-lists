c9f104f9ad1760b00916cb480ea6a2ead620a4ca
Vinicius Santoro <vini.santoro@gmail.com>
2025-06-04 08:57:57 -0300

    Fix Firebase storage bucket URL

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBeAVHfvUU993JQHJLMc9BHsV2KUZOs33U",
     authDomain: "my-list-2f3a7.firebaseapp.com",
     projectId: "my-list-2f3a7",
-    storageBucket: "my-list-2f3a7.firebasestorage.app",
+    storageBucket: "my-list-2f3a7.appspot.com",
     messagingSenderId: "515380103480",
     appId: "1:515380103480:web:0d6c2e7d4ee47cc9d299cd",
     measurementId: "G-V0K1T22ZNB"
