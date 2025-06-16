2ef3bb9296036ef45db2144dc7469e9f042a7fbe
vinisantoro <vini.santoro@gmail.com>
2025-06-16 11:42:59 -0300

    cleanup folder

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBeAVHfvUU993JQHJLMc9BHsV2KUZOs33U",
-    authDomain: "my-list-2f3a7.firebaseapp.com",
-    projectId: "my-list-2f3a7",
-    storageBucket: "my-list-2f3a7.firebasestorage.app",
-    messagingSenderId: "515380103480",
-    appId: "1:515380103480:web:0d6c2e7d4ee47cc9d299cd",
-    measurementId: "G-V0K1T22ZNB"
-    };
